3ec09b2a41cfafc826c7506ffe2d67b5b197091d
Tobias Behr <15959166+TobiasB90@users.noreply.github.com>
2019-02-25 19:52:13 +0100

    Initial Commit / Prototype state

---
+++ SmartMailbox/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour
+{
+    [Header("Connection Settings")]
+    public int ArduinoID;
+    public string ServerIP;
+    public string ServerPHP;
+
+    [Header("Misc")]
+    [SerializeField] private Image LatestMailPic;
+    [SerializeField] private GameObject NewMailObj;
+    [SerializeField] private GameObject NoMailObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetImage();
+    }
+
+    public void GetImage()
+    {
+        if(ServerIP != null && ServerPHP != null)
+        {
+            string ServerURL = ServerIP + "/" + ServerPHP;
+            StartCoroutine(GetImageURL(ServerURL));
+        }        
+    }
+
+    IEnumerator GetImageURL(string ServerURL)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("ArduinoID", ArduinoID);
+        form.AddField("action", "getimage");
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(ServerURL, form))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = ServerURL.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                StartCoroutine(GetImageData(webRequest.downloadHandler.text));
+            }
+        }
+    }
+
+    IEnumerator GetImageData(string imageURL)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(imageURL))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+                NoMailObj.SetActive(true);
+            }
+            else
+            {
+                var texture = DownloadHandlerTexture.GetContent(uwr);
+                var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+
+                if(LatestMailPic != null)
+                {
+                    LatestMailPic.sprite = sprite;
+                    NoMailObj.SetActive(false);
+                    NewMailObj.SetActive(true);
+                }
+            }
+        }
+    }
+}
---
+++ SmartMailbox/Assets/Scripts/UIManager.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField] private GameObject HomeContent;
+    [SerializeField] private GameObject HistoryContent;
+    [SerializeField] private GameObject SettingsContent;
+
+    [SerializeField] private Color UIButtonBar_active;
+    [SerializeField] private Color UIButtonBar_inactive;
+
+    [SerializeField] private TextMeshProUGUI HomeIcon;
+    [SerializeField] private TextMeshProUGUI HomeText;
+    [SerializeField] private GameObject NewMailObj;
+    [SerializeField] private GameObject NoMailObj;
+
+    [SerializeField] private TextMeshProUGUI HistoryIcon;
+    [SerializeField] private TextMeshProUGUI HistoryText;
+
+    [SerializeField] private TextMeshProUGUI SettingsIcon;
+    [SerializeField] private TextMeshProUGUI SettingsText;
+    [SerializeField] private GameObject SettingsSavedText;
+
+    [SerializeField] private TextMeshProUGUI Settings_InputField_ArduinoID;
+    [SerializeField] private TextMeshProUGUI Settings_InputField_ServerURL;
+    [SerializeField] private TextMeshProUGUI Settings_InputField_ServerPHP;
+
+    [SerializeField] NetworkManager networkManager;
+
+    public void Start()
+    {
+        // Initialie home screen at the start
+        initHomescreen();
+    }
+
+    public void initHomescreen()
+    {
+        // Set all other UI Colors to inactive
+        HistoryIcon.color   = UIButtonBar_inactive;
+        HistoryText.color   = UIButtonBar_inactive;
+        SettingsIcon.color  = UIButtonBar_inactive;
+        SettingsText.color  = UIButtonBar_inactive;
+
+        // Set the home screen button color to active
+        HomeIcon.color = UIButtonBar_active;
+        HomeText.color = UIButtonBar_active;
+
+        // Set the other contents as inactive
+        HistoryContent.SetActive(false);
+        SettingsContent.SetActive(false);
+        NoMailObj.SetActive(false);
+
+        // Set the home content as active
+        HomeContent.SetActive(true);
+
+        // Get the newest mail
+    }
+
+    public void initHistoryscreen()
+    {
+        // Set all other UI Colors to inactive
+        HomeIcon.color = UIButtonBar_inactive;
+        HomeText.color = UIButtonBar_inactive;
+        SettingsIcon.color = UIButtonBar_inactive;
+        SettingsText.color = UIButtonBar_inactive;
+
+        // Set the history screen button color to active
+        HistoryIcon.color = UIButtonBar_active;
+        HistoryText.color = UIButtonBar_active;
+
+        // Set the other contents as inactive
+        HomeContent.SetActive(false);
+        SettingsContent.SetActive(false);
+
+        // Set the home content as active
+        HistoryContent.SetActive(true);
+    }
+
+    public void initSettingsscreen()
+    {
+        // Set all other UI Colors to inactive
+        HistoryIcon.color = UIButtonBar_inactive;
+        HistoryText.color = UIButtonBar_inactive;
+        HomeIcon.color = UIButtonBar_inactive;
+        HomeText.color = UIButtonBar_inactive;
+        SettingsSavedText.SetActive(false);
+
+        // Set the home screen button color to active
+        SettingsIcon.color = UIButtonBar_active;
+        SettingsText.color = UIButtonBar_active;
+
+        // Set the other contents as inactive
+        HistoryContent.SetActive(false);
+        HomeContent.SetActive(false);
+
+        // Set the settings content as active
+        SettingsContent.SetActive(true);
+
+        // Get the current networking settings and display them
+        Settings_InputField_ArduinoID.text = networkManager.ArduinoID.ToString();
+        Settings_InputField_ServerURL.text = networkManager.ServerIP;
+        Settings_InputField_ServerPHP.text = networkManager.ServerPHP;
+    }
+
+    public void SaveSettings()
+    {
+        // Save the inputs to the variables from the networkmanager
+        networkManager.ArduinoID    = int.Parse(Settings_InputField_ArduinoID.text);
+        networkManager.ServerIP     = Settings_InputField_ServerURL.text;
+        networkManager.ServerPHP    = Settings_InputField_ServerPHP.text;
+        SettingsSavedText.SetActive(true);
+    }
+}
